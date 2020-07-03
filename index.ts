7663c7f5e8760efc0ec043a951774d38a93e84bb
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2020-07-03 18:42:56 +0200

    3 files modified

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
 import './style.scss';
 
 //document.getElementById('app').innerHTML = "<h1>Hello World</h1>";
+
+const str= " ";
+const str2 = " ";
+
+console.log(str.charCodeAt(0));
+console.log(str2.charCodeAt(0));
+
