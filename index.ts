531a7320c3b7d344657d0577ce2cb903c9de7580
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2020-08-19 16:14:55 +0200

    3 files modified

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
 import './style.scss';
 
+const form= document.getElementById( "my_form" ) as HTMLFormElement;
+
+form
+.addEventListener("submit", (e) => {
+
+  console.log("wesh");
+
+  const { elements }= e.target as any;
+
+  console.log(elements.title.value);
+
+  e.preventDefault();
+
+})
